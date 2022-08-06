41569c67ae6e5a0f4379f335309aabe4132a6555
KeyurMistry <keyurmist@gmail.com>
2022-08-06 11:12:32 +0100

    added get and set in user.js

--- 22-Stu_Virtuals/Unsolved/models/User.js
+++ 22-Stu_Virtuals/Unsolved/models/User.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
@@ -15,12 +15,21 @@ const userSchema = new Schema(
 );
 
 // TODO: Create a virtual property `fullName` on the userSchema
-
+userSchema
+  .virtual(`fullName`)
+  .get(function () {
+    return `${this.first} ${this.last}`;
+  })
+  .set(function (fullName) {
+    const names = fullName.split(" ");
+    this.first = names[0];
+    this.last = names[1];
+  });
 // TODO: Create a getter for the virtual that returns the full name of the user (first + last)
 
 // TODO: Create a setter for the virtual that sets the value of the first and last name, given just the `fullName`
 
 // Initialize our User model
-const User = model('user', userSchema);
+const User = model("user", userSchema);
 
 module.exports = User;
